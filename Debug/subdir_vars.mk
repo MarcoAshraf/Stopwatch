9a697186e17c2f390c92aeca0a30fe7bd172013f
MarcoAshraf <marco.ashraf.alfy@gmail.com>
2023-08-27 19:19:31 +0300

    .

--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -12,64 +12,52 @@ C_SRCS += \
 ../BITWISE_OPERATIONS.c \
 ../DIO.c \
 ../GPIO_INTERRUPT.c \
-../Keypad.c \
 ../LCD.c \
 ../Systick.c \
 ../main.c \
-../parse.c \
 ../tm4c123gh6pm_startup_ccs.c 
 
 C_DEPS += \
 ./BITWISE_OPERATIONS.d \
 ./DIO.d \
 ./GPIO_INTERRUPT.d \
-./Keypad.d \
 ./LCD.d \
 ./Systick.d \
 ./main.d \
-./parse.d \
 ./tm4c123gh6pm_startup_ccs.d 
 
 OBJS += \
 ./BITWISE_OPERATIONS.obj \
 ./DIO.obj \
 ./GPIO_INTERRUPT.obj \
-./Keypad.obj \
 ./LCD.obj \
 ./Systick.obj \
 ./main.obj \
-./parse.obj \
 ./tm4c123gh6pm_startup_ccs.obj 
 
 OBJS__QUOTED += \
 "BITWISE_OPERATIONS.obj" \
 "DIO.obj" \
 "GPIO_INTERRUPT.obj" \
-"Keypad.obj" \
 "LCD.obj" \
 "Systick.obj" \
 "main.obj" \
-"parse.obj" \
 "tm4c123gh6pm_startup_ccs.obj" 
 
 C_DEPS__QUOTED += \
 "BITWISE_OPERATIONS.d" \
 "DIO.d" \
 "GPIO_INTERRUPT.d" \
-"Keypad.d" \
 "LCD.d" \
 "Systick.d" \
 "main.d" \
-"parse.d" \
 "tm4c123gh6pm_startup_ccs.d" 
 
 C_SRCS__QUOTED += \
 "../GPIO_INTERRUPT.c" \
-"../Keypad.c" \
 "../LCD.c" \
 "../Systick.c" \
 "../main.c" \
-"../parse.c" \
 "../tm4c123gh6pm_startup_ccs.c" 
 
 
--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include "DIO.h"
 #include "GPIO_INTERRUPT.h"
 #include "LCD.h"
-#include "Keypad.h"
-#include "parse.h"
 #include "stdlib.h"
 #include "stdio.h"
 #include "Systick.h"
@@ -10,13 +8,7 @@
 #define PUSHBUTTON1 0
 #define PUSHBUTTON2 4
 
-extern int LCD_pos;
-extern int LCD_counter;
 
-uint8_t counter = 0;
-
-float parse(char *s);
-char s[100];
 
 volatile uint8_t button = 0;
 
@@ -24,10 +16,9 @@ volatile uint8_t button = 0;
 
 int main(void)
 {
-    Keypad_INIT();
     delay_ms(50);
     LCD_INIT();
-    SEND_DATA(':');
+
 
     DIO_INIT(PORTF, 0b00000000, 0b00010001);
     interrupt_init(PORTF, PUSHBUTTON1);
@@ -84,103 +75,3 @@ void SYSTICK_isr()
 }
 
 
-
-/*
-//
-//void Read_Keypad()
-//{
-//        uint8_t Keypad[4][4]=   {{'1','2','3','+'},
-//                                {'4','5','6','-'},
-//                                {'7','8','9','*'},
-//                                {'#','0','=','/'}}; // Initializing array to loop on it
-//        // Clearing the interrupt Flag
-//        GPIO_PORTB_ICR_R=0xFF;
-//        uint8_t row = 0;
-//        uint8_t col = 0;
-//        for (row = 0; row < 4; row++)
-//        {
-//            int temp = bit_check(GPIO_PORTB_DATA_R, row);
-//            if(!temp)
-//            {
-//                break;
-//            }
-//        }
-//
-//
-//        for (col = 4; col < 8; col++)
-//        {
-//            bitset(GPIO_PORTB_DATA_R, col);
-//
-//            if (bit_check(GPIO_PORTB_DATA_R, row))
-//            {
-//                //reset the state of the columns back to 0
-//                bitclear(GPIO_PORTB_DATA_R, 4);
-//                bitclear(GPIO_PORTB_DATA_R, 5);
-//                bitclear(GPIO_PORTB_DATA_R, 6);
-//                bitclear(GPIO_PORTB_DATA_R, 7);
-//                if(row!=4 &&Keypad[row][col-4]!= '=')
-//                {
-//                    if(Keypad[row][col-4] == '#') // Reset
-//                    {
-//                        clear_display();
-//                        return_home();
-//                        LCD_pos = 0;
-//                        LCD_counter = 0;
-//                        counter = 0;
-//                    }
-//                    else
-//                    {
-//                    SEND_DATA(Keypad[row][col-4]);
-//                    s[counter] = Keypad[row][col-4];
-//                    counter++;
-//                    }
-//                }
-//                else if(Keypad[row][col-4]== '=')
-//                {
-//                    float temp = parse(s);
-//                    /*
-//                    if((int)temp == (int)SyntaxError)
-//                    {
-//                        clear_display();
-//                        LCD_pos = 0;
-//                        LCD_counter = 0;
-//                        SEND_STRING("Syntax Error");
-//                        delay_ms(1000);
-//                        clear_display();
-//                        return_home();
-//                        LCD_pos = 0;
-//                        LCD_counter = 0;
-//                        counter = 0;
-//
-//                    }
-//
-//                    else if(((int)temp == (int)MathError) < 1e-9)
-//                    {
-//                        clear_display();
-//                        LCD_pos = 0;
-//                        LCD_counter = 0;
-//                        SEND_STRING("Math Error");
-//                        delay_ms(1000);
-//                        clear_display();
-//                        return_home();
-//                        LCD_pos = 0;
-//                        LCD_counter = 0;
-//                        counter = 0;
-//                    }
-//                    else
-//                   {*/
-//                       // sprintf(s, "%f", parse(s));
-//                        ltoa((int)temp, s);
-//                        clear_display();
-//                        SEND_STRING(s);
-//                  // }
-//                }
-//                delay_ms(50);
-//                GPIO_PORTB_ICR_R=0xFF;
-//                SETBIT(NVIC_UNPEND0_R, 1);
-//                return;
-//            }
-//
-//        }
-//
-//}*/

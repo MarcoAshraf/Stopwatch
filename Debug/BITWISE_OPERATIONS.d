3f8862c7a93eb55f8b8e6a32021a844f7d3fcfe4
MarcoAshraf <marco.ashraf.alfy@gmail.com>
2023-08-29 09:05:46 +0300

    .

---
+++ Debug/BITWISE_OPERATIONS.d
@@ -0,0 +1,9 @@
+# FIXED
+
+BITWISE_OPERATIONS.obj: ../BITWISE_OPERATIONS.c
+BITWISE_OPERATIONS.obj: ../BITWISE_OPERATIONS.h
+BITWISE_OPERATIONS.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+
+../BITWISE_OPERATIONS.c: 
+../BITWISE_OPERATIONS.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
